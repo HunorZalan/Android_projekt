b505e3a32532a5f7ed8e12664df8423bc61c083c
HunorZalan <nagyhunor9@gmail.com>
2020-11-17 20:19:23 +0200

    Added FragmentList commit

--- app/src/main/java/com/example/projekt/SplashScreen.kt
+++ app/src/main/java/com/example/projekt/SplashScreen.kt
@@ -20,7 +20,7 @@ class SplashScreen : AppCompatActivity() {
 
         //image = findViewById(R.id.img)
         img.alpha = 0f
-        img.animate().setDuration(5000).alpha(1f).withEndAction{
+        img.animate().setDuration(7000).alpha(1f).withEndAction{
             val i = Intent(this, MainActivity::class.java)
             startActivity(i)
             overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
