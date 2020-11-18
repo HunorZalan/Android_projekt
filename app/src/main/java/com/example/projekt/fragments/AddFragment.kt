5f9b0c3ca736af7fa21dd4712c3126cae79decd0
HunorZalan <nagyhunor9@gmail.com>
2020-11-18 02:06:43 +0200

    AddFragment added commit

--- app/src/main/java/com/example/projekt/fragments/AddFragment.kt
+++ app/src/main/java/com/example/projekt/fragments/AddFragment.kt
@@ -6,7 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.projekt.R
+import kotlinx.android.synthetic.main.fragment_add.view.*
 
 class AddFragment : Fragment() {
 
@@ -19,6 +21,24 @@ class AddFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         var view : View = inflater.inflate(R.layout.fragment_add, container, false)
+
+        view.add_button.setOnClickListener{
+            var ok : Boolean = true
+            if (view.add_name.text.isEmpty()){
+                view.add_name.error = "Empty"
+                ok = false
+            }
+
+            if (view.add_calorie.text.isEmpty()){
+                view.add_calorie.error = "Empty"
+                ok = false
+            }
+
+            if (ok == true){
+                Navigation.findNavController(view).navigate(R.id.action_addFragment_to_listFragment)
+            }
+
+        }
 
         return view
     }
